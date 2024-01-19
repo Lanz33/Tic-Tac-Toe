e8f4ed82edfacc12e49495a7072a03d61eb1af45
Christian Fischer <lanz33@gmail.com>
2024-01-19 10:57:19 +0100

    Grid and animation done, next define onclick-Event

--- script.js
+++ script.js
@@ -19,11 +19,39 @@ function render() {
         tableHTML += '<tr>';
         for (let j = 0; j < 3; j++) {
             let symbol = fields[index++];
-            tableHTML += `<td>${symbol === 'cross' ? 'X' : symbol === 'circle' ? 'O' : ''}</td>`;
+            tableHTML += `<td>${symbol === 'cross' ? generateCrossSVG() : symbol === 'circle' ? generateCircleSVG() : ''}</td>`;
         }
         tableHTML += '</tr>';
     }
 
     tableHTML += '</table>';
     contentDiv.innerHTML = tableHTML;
+}
+
+function generateCircleSVG() {
+    const svgCode = /*html*/`
+        <svg width="70" height="70">
+            <circle cx="30" cy="30" r="25" fill="transparent" stroke="#00B0EF" stroke-width="5" >
+                <animate attributeName="stroke-dasharray" values="0,219;219,219" dur="0.4s" keyTimes="0;1" />
+            </circle>
+        </svg>
+    `;
+    return svgCode;
+}
+
+function generateCrossSVG() {
+    const svgCode = /*html*/`
+        <svg width="70" height="70">
+            <line x1="5" y1="5" x2="65" y2="65" stroke="#FFC000" stroke-width="5">
+                <animate attributeName="x2" values="0;70" dur="0.2s" begin="0s" />
+                <animate attributeName="y2" values="0;70" dur="0.2s" begin="0s" />
+            </line>
+            <line x1="65" y1="5" x2="5" y2="65" stroke="#FFC000" stroke-width="5">
+                <animate attributeName="x2" values="70;0" dur="0.2s" begin="0.2s" />
+                <animate attributeName="y2" values="0;70" dur="0.2" begin="0.2s" />
+            </line>
+        </svg>
+    `;
+
+    return svgCode;
 }
